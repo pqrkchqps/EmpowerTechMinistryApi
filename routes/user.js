4f0dda9281fddef1cdf761abcdb81919e2a30887
John <johnacreps@gmail.com>
2024-08-17 11:23:50 -0500

    making get user by email endpoint require auth

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const userController = require("../controllers/userController");
 
-
-router.get("/:email", userController.getUser);
+router.get("/:email", auth, userController.getUser);
 
 module.exports = router;
