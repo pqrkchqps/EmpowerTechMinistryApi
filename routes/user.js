1c36d41ca026d128708fe34141f642e65fb2d59e
John <johnacreps@gmail.com>
2024-09-30 11:51:28 -0500

    made getting the author info not require auth

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const userController = require("../controllers/userController");
 
-router.get("/:email", auth, userController.getUser);
+router.get("/:email", userController.getUser);
 router.put("/update", auth, userController.updateUser);
 router.put("/thread/readcount", auth, userController.setThreadReadCount);
 router.put("/article/readcount", auth, userController.setArticleReadCount);
