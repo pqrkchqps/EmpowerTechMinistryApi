46ebe09e2352ee01542c47fbc322b8f2a1f7d9ea
John <johnacreps@gmail.com>
2025-03-21 14:13:52 -0500

    changing the route order for getArticleById

--- routes/article.js
+++ routes/article.js
@@ -6,7 +6,7 @@ const { checkAdmin, checkGeneral } = require("../middleware/checkUser");
 
 router.post("/", auth, checkGeneral, articleController.createArticle);
 router.put("/:id", auth, checkGeneral, articleController.editArticle);
-router.get("/", articleController.getAllArticles);
 router.get("/:id", articleController.getArticleById);
+router.get("/", articleController.getAllArticles);
 
 module.exports = router;
